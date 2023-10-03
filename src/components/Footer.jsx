b30634de7b761fb6acfa3ae5449fb4115452f650
anandkumar-1998 <anand.kumar@nidester.com>
2023-10-03 14:41:18 +0530

    semi complete mockup

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,58 +4,84 @@ const Footer = () => {
   return (
     <footer className="bg-primary text-white text-center text-lg-start my-3">
       {/* <!-- Grid container --> */}
-       <div className="container p-4">
-       {/* <!--Grid row-->
+      <div className="container p-4">
+        {/* <!--Grid row--> */}
         <div className="row">
-          <!--Grid column-->
+          {/*<!--Grid column-->*/}
           <div className="col-lg-6 col-md-12 mb-4 mb-md-0">
-            {/* <h5 className="text-uppercase">Footer Content</h5> */}
-
-            {/* <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste
-              atque ea quis molestias. Fugiat pariatur maxime quis culpa
-              corporis vitae repudiandae aliquam voluptatem veniam, est atque
-              cumque eum delectus sint!
-            </p> */}
-          {/* </div> */}
+            <div className="row">
+              <div className="col-xs-12 col-sm-6">
+                <h5 className="text-uppercase">CBC Office</h5>
+                <p>
+                  Capacity building Commission , 22nd Floor, Jawahar Vyapar
+                  Bhawan, Tolstoy Rd, HC Mathur Lane, New Delhi, Delhi 110001
+                </p>
+              </div>
+              <div className="col-xs-12 col-sm-6">
+                <h5 className="text-uppercase">CBC Map</h5>
+                <img className="img-fluid" src="map_small.jpg" alt="..." />
+              </div>
+            </div>
+          </div>
           {/* {/* <!--Grid column--> */}
-
-      {/* <!--Grid column--> */}
+          <div className="col-lg-6 col-md-12 mb-4 mb-md-0 d-none"></div>
+          {/* <!--Grid column--> */}
           <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
             <h5 className="text-uppercase">Quick Link</h5>
             <ul className="list-unstyled mb-0 ">
               <li>
-                <a href="https://cbc.gov.in/" className="text-white text-decoration-none">
+                <a
+                  href="https://cbc.gov.in/"
+                  className="text-white text-decoration-none"
+                >
                   Home
                 </a>
               </li>
               <li>
-                <a href="https://cbc.gov.in/acbp" className="text-white text-decoration-none">
+                <a
+                  href="https://cbc.gov.in/acbp"
+                  className="text-white text-decoration-none"
+                >
                   About CBC
                 </a>
               </li>
               <li>
-                <a href="https://cbc.gov.in/mandate-of-the-commission" className="text-white text-decoration-none">
+                <a
+                  href="https://cbc.gov.in/mandate-of-the-commission"
+                  className="text-white text-decoration-none"
+                >
                   CBC Mandate
                 </a>
               </li>
               <li>
-                <a href="https://dopttrg.nic.in/igotmk/" className="text-white text-decoration-none">
-                Mission Karmayogi
+                <a
+                  href="https://dopttrg.nic.in/igotmk/"
+                  className="text-white text-decoration-none"
+                >
+                  Mission Karmayogi
                 </a>
               </li>
               <li>
-                <a href="https://cbc.gov.in/sites/default/files/ACBP_Approach-Paper.pdf" className="text-white text-decoration-none">
+                <a
+                  href="https://cbc.gov.in/sites/default/files/ACBP_Approach-Paper.pdf"
+                  className="text-white text-decoration-none"
+                >
                   Approach Paper
                 </a>
               </li>
               <li>
-                <a href="https://igotkarmayogi.gov.in/" className="text-white text-decoration-none">
-                  IGOT 
+                <a
+                  href="https://igotkarmayogi.gov.in/"
+                  className="text-white text-decoration-none"
+                >
+                  IGOT
                 </a>
               </li>
               <li>
-                <a href="https://cbc.gov.in/contact-us" className="text-white text-decoration-none">
+                <a
+                  href="https://cbc.gov.in/contact-us"
+                  className="text-white text-decoration-none"
+                >
                   Contact us
                 </a>
               </li>
@@ -93,10 +119,10 @@ const Footer = () => {
           {/* <!--Grid column--> */}
         </div>
         {/* <!--Grid row--> */}
-      {/*</div> */}
-       {/* <!-- Grid container --> */}
+      </div>
+      {/* <!-- Grid container --> */}
 
-  {/* <!-- Copyright --> */}
+      {/* <!-- Copyright --> */}
       <div className="text-center p-3" style={{ backgroundColor: "darkblue" }}>
         © 2023 Copyright:
         <a
